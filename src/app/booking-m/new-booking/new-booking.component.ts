e79e7b92a3e9e2d2b8c286abe82fc6ea46642481
Matin Mahmood <matin.mahmood99@gmail.com>
2020-11-07 03:06:04 +0000

    nothing

--- src/app/booking-m/new-booking/new-booking.component.ts
+++ src/app/booking-m/new-booking/new-booking.component.ts
@@ -1,37 +1,57 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {
-  AgendaService,
-  DayService,
-  MonthAgendaService,
-  MonthService, TimelineMonthService,
-  TimelineViewsService,
-  WeekService,
-  WorkWeekService
-} from '@syncfusion/ej2-angular-schedule';
-import {$e} from 'codelyzer/angular/styles/chars';
-import {ScheduleComponent} from '@syncfusion/ej2-angular-schedule';
+import { ScheduleComponent, EventSettingsModel, DayService, WeekService, WorkWeekService, MonthService, View } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-new-booking',
-  templateUrl: './new-booking.component.html',
+  providers: [DayService, WeekService, WorkWeekService, MonthService],
+  template: `<button #addButton ejs-button id="addButton" type="button" content="Add" (click)="onButtonClick()"></button>
+  <ejs-schedule #scheduleObj width='100%' height='520px' [selectedDate]="selectedDate" [eventSettings]="eventSettings" [views]="scheduleViews"></ejs-schedule>`,
   styleUrls: ['./new-booking.component.scss']
 })
 export class NewBookingComponent implements OnInit {
 
   constructor() { }
 
+  ngOnInit(): void {
+    }
+
   dateTodisplay = new Date(2020, 10, 11);
   newBookings = [];
 
-  @ViewChild('scheduleObj')
+  @ViewChild("scheduleObj")
   public scheduleObj: ScheduleComponent;
-
-  ngOnInit(): void {
-
+  @ViewChild("addButton")
+  public selectedDate: Date = new Date(2018, 1, 15);
+  public scheduleViews: View[] = ['Day', 'Week', 'WorkWeek', 'Month'];
+  public eventSettings: EventSettingsModel = {
+    dataSource: [{
+      Id: 1,
+      Subject: 'Testing',
+      StartTime: new Date(2018, 1, 11, 9, 0),
+      EndTime: new Date(2018, 1, 11, 10, 0),
+      IsAllDay: false
+    }, {
+      Id: 2,
+      Subject: 'Vacation',
+      StartTime: new Date(2018, 1, 13, 9, 0),
+      EndTime: new Date(2018, 1, 13, 10, 0),
+      IsAllDay: false
+    }]
   }
-
-  afterContentInit(): void {
-    this.scheduleObj.eventRendered.subscribe(x => {console.log(x); });
+  public onButtonClick(): void {
+    let data: Object[] = [{
+      Id: 3,
+      Subject: 'Conference',
+      StartTime: new Date(2018, 1, 12, 9, 0),
+      EndTime: new Date(2018, 1, 12, 10, 0),
+      IsAllDay: true
+    }, {
+      Id: 4,
+      Subject: 'Meeting',
+      StartTime: new Date(2018, 1, 15, 10, 0),
+      EndTime: new Date(2018, 1, 15, 11, 30),
+      IsAllDay: false
+    }];
+    this.scheduleObj.addEvent(data);
   }
-
 }
