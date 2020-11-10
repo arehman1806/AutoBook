1a974cdc7633dd8ac8c60899f88bb62b2d7593aa
arehman1806 <arehmannasir@outlook.com>
2020-11-10 20:57:11 +0000

    firebaseADMIN.py created and app initialized

---
+++ autobackend/firebaseADMIN.py
@@ -0,0 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("path/to/service_account_key")
+app = firebase_admin.initialize_app(cred)
+
+firestore_client = firebase_admin.firestore.client()
+firestore_client
