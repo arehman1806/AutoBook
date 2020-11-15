860f159278120a5ad1ca6f03161a244cfc63f83f
Matin Mahmood <matin.mahmood99@gmail.com>
2020-11-15 00:45:38 +0000

    cors

--- autobackend/app.py
+++ autobackend/app.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 import pyautogui as pyautogui
 from flask import Flask, jsonify
 from flask import request
+from flask_cors import CORS
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
@@ -14,6 +15,15 @@ import firebaseADMIN
 import plgym
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/new_platform', methods=['POST'])
+def new_platform():
+  platform_data_json = request.get_json()
+  print(platform_data_json)
+
+  return jsonify(message='OK')
 
 
 @app.route('/new_booking/<string:uid>/<int:did>')
