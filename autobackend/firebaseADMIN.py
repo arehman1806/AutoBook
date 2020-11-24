c596fad7bbd7fc13bc55c824b032d80117980791
arehman1806 <arehmannasir@outlook.com>
2020-11-24 20:48:20 +0000

    new platform can now be added

--- autobackend/firebaseADMIN.py
+++ autobackend/firebaseADMIN.py
@@ -19,7 +19,13 @@ def fetch_booking_data(uid: str, did: str):
   return booking_data_json
 
 
-def save_platform_connect_data(uid: str, did: str):
+def save_platform_connect_data(uid: str, platform_id: str, platform_username: str, platform_password: str):
+  userProfileRef = 'users/' + uid
+  userPlatformRef = 'users/' + uid + '/platforms/' + platform_id
+  userProfileConnectedPlatforms = firestore_client.document(userProfileRef).get().to_dict()['connectedPlatforms']
+  userProfileConnectedPlatforms.append(platform_id)
+  firestore_client.document(userPlatformRef).set({'username': platform_username, 'password': platform_password})
+  firestore_client.document(userProfileRef).update({'connectedPlatforms': userProfileConnectedPlatforms})
   return True
 
 # if __name__ == '__main__':
