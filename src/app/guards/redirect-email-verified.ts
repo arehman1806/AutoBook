fac45bee781d3e8a35d7ff20a36f7b2aa939c9b6
arehman1806 <arehmannasir@outlook.com>
2020-11-29 21:06:21 +0000

    missing route guard file added

---
+++ src/app/guards/redirect-email-verified.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedirectEmailVerified implements CanActivate {
+  constructor(private afAuth: AngularFireAuth) {
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.afAuth.user.pipe(
+      map(
+        (user) => {
+          return user && !user.emailVerified;
+        }
+      )
+    );
+  }
+
+}
