866def82bb1acf3c7d4e145bbf6fc45715c2f6b2
arehman1806 <arehmannasir@outlook.com>
2020-11-29 01:12:56 +0000

    new home page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {CalenderComponent} from './calender/calender.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'calender', component: CalenderComponent},
   {path: 'booking',
     loadChildren: () => import ('./booking-m/booking.module').then(m => m.BookingModule)},
   {path: '', redirectTo: 'booking', pathMatch: 'full'}
