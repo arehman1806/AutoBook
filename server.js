3515add2ba5c6814a7357268983a172ebcb8571e
Matin Mahmood <matin.mahmood99@gmail.com>
2020-12-29 15:44:39 +0000

    fix dist dir

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(compression())
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/autobookme'));
+app.use(express.static('./dist/AutoBook'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/autobookme/'}),
+  res.sendFile('index.html', {root: 'dist/AutoBook/'}),
 );
 
 // Start the app by listening on the default Heroku port
